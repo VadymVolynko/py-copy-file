2607e99630e2b75fefb6d46f70f9fee5887dbbc3
VadymVolynko <world.ow1@ukr.net>
2026-01-06 19:43:05 +0200

    Версия 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, src, dst = parts
+
+    if src == dst:
+        return
+
+    with open(src, "r") as file_in, open(dst, "w") as file_out:
+        file_out.write(file_in.read())
+
